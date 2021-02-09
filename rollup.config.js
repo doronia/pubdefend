e525007f7c19a2fe9b58c1d891184de34200c0df
Doron M <doron.info@gmail.com>
2021-02-09 09:54:06 +0200

    123

--- rollup.config.js
+++ rollup.config.js
@@ -11,13 +11,45 @@ const ANALYZE = process.env.ANALYZE ? process.env.ANALYZE === "true" : false;
 
 const publisher = isProduction ? "/" + DOMAIN + "/" : "";
 
-const terserOptions = {
-	compress: {
-		passes: 2,
+const terserOptions_v2 = {
+	parse: {
+		// parse options
 	},
+	compress: {
+		// compress options
+		//drop_console: true,
+	},
+	mangle: {
+		// mangle options
+
+		properties: {
+			reserved: ["googletag", "pubads", "errorMessage", "getSlots", "slot", "getSlotElementId", "client", "timeout", "userName", "password", "willMessage", "keepAliveInterval", "cleanSession", "useSSL", "invocationContext", "onSuccess", "onFailure", "hosts", "ports", "mqttVersion", "onMessageArrived", "onConnectionLost", "qos", "invocationContext", "destinationName"],
+			keep_quoted: true,
+		},
+	},
+	format: {
+		comments: false,
+		preamble: "/* pubdefend v0.01 */",
+	},
+
+	ecma: 5, // specify one of: 5, 2015, 2016, etc.
+	keep_classnames: false,
+	keep_fnames: false,
+	ie8: false,
+	module: false,
+	nameCache: null, // or specify a name cache object
+	safari10: false,
+	toplevel: true,
+};
+const terserOptions = {
+	toplevel: true,
+	/* compress: {
+		passes: 1,
+	}, */
 	mangle: {
 		safari10: true,
 		properties: {
+			reserved: ["googletag", "pubads", "getSlots", "slot", "getSlotElementId", "client", "timeout", "userName", "password", "willMessage", "keepAliveInterval", "cleanSession", "useSSL", "invocationContext", "onSuccess", "onFailure", "hosts", "ports", "mqttVersion", "onMessageArrived", "onConnectionLost"],
 			keep_quoted: true,
 		},
 	},
@@ -34,19 +66,26 @@ module.exports = [
 				file: "dist/pubDefend.iife.js",
 				name: "pubdefend",
 				format: "iife",
+				banner: "/* pubDefend version " + 1.01 + " */",
 			},
 			{
 				file: "dist/" + publisher + "pubDefend.iife.min.js",
 				name: "pubdefend",
 				format: "iife",
-				plugins: [terser(terserOptions)],
+				plugins: [terser(terserOptions_v2)],
+			},
+			{
+				file: "dist/" + publisher + "pubDefend.iife.min.js",
+				name: "pubdefend",
+				format: "iife",
+				plugins: [terser(terserOptions_v2)],
 			},
 		],
 		plugins: [
 			resolve({
 				browser: true,
 			}),
-			babel({
+			/* babel({
 				exclude: "node_modules/**",
 				babelrc: false,
 				presets: [
@@ -63,7 +102,7 @@ module.exports = [
 						},
 					],
 				],
-			}),
+			}), */
 			commonjs(),
 			replace({
 				exclude: "node_modules/**",

bc507060e98dc52359670a4e98b00b579789f261
Doron M <doron.info@gmail.com>
2021-02-15 09:31:19 +0200

    תיקונים

--- src/pubdefend.google.js
+++ src/pubdefend.google.js
@@ -1,6 +1,8 @@
 import { pd } from "./pubdefend.init";
 import { store } from "./pubdefend.events";
 import { domQuery, checkIfVisible } from "./pubdefend.dom";
+import { isArray } from "./pubdefend.polyfills";
+import { customEvent } from "./pubdefend.events";
 
 var _store = pd.store;
 var solts_arr = {},
@@ -23,84 +25,61 @@ export function Slots() {
 }
 
 export function gtagHandler(g, callback) {
-	var getslots = Slots();
-	var getAllEvents = window.googletag.getEventLog().getAllEvents();
+	var gtag = window["googletag"];
 
-	listenForSlotsCallback(getAllEvents);
-	console.debug("getAllEvents", getAllEvents);
-	solts_req = g.pubads().getSlots().length;
-	console.log("googaltag slots:", solts_req);
+	/* googletag defined Slots */
+	solts_req = googletag.pubads().getSlots().length;
 	store(_store, "gtag_slots", solts_req);
+	console.log("googaltag slots:", solts_req);
 
-	/* listenForSlots */
-	//if (!_store.ab) {
-	//console.debug("pubdefend:: No adblocker detected. listen to gtag");
-	//g.addEventListener("slotRenderEnded", listenForSlots, false);
+	Listener = gtag.pubads().addEventListener("slotRenderEnded", listenForSlots.bind(null, listenForSlotsCallback), false);
 
-	/* googletag.pubads().addEventListener("slotRenderEnded", function (event) {
-		var slot = event.slot;
-		console.group("Slot", slot.getSlotElementId(), "finished rendering.");
-
-		// Log details of the rendered ad.
-		console.log("Advertiser ID:", event.advertiserId);
-		console.log("Campaign ID: ", event.campaignId);
-		console.log("Creative ID: ", event.creativeId);
-		console.log("Is empty?:", event.isEmpty);
-		console.log("Line Item ID:", event.lineItemId);
-		console.log("Size:", event.size);
-		console.log("Source Agnostic Creative ID:", event.sourceAgnosticCreativeId);
-		console.log("Source Agnostic Line Item ID:", event.sourceAgnosticLineItemId);
-		console.groupEnd();
-	}); */
-
-	//Listener = g.addEventListener("slotRenderEnded", listenForSlots.bind(null, listenForSlotsCallback), false);
-	//} else {
 	if (callback) {
 		callback("blocked");
 	}
-	//}
 
 	return;
 }
 
-//var listenForSlots = function (callback, event) {
-var listenForSlots = function (event) {
+var listenForSlots = function (callback, event) {
+	//var listenForSlots = function (event) {
 	var slot = event.slot;
-	console.log(slot);
+	//console.log(slot);
 
 	var id = slot.getSlotElementId();
 	var elm = document.getElementById(id);
-	var isItVisible = checkIfVisible(elm);
-	solts_arr[id] = true;
+	//var isItVisible = checkIfVisible(elm);
+	//solts_arr[id] = true;
 	//solts_arr[id] = { render: true, visible: isItVisible };
 
-	console.log("googaltag Slot", slot.getSlotElementId(), "finished rendering.");
+	console.group("Slot", slot.getSlotElementId(), "finished rendering.");
+	console.log("Is empty?:", event.isEmpty);
+	console.log("Size:", event.size);
+	console.groupEnd();
 	//console.log('Slot', slot.getSlotElementId(), 'visibility:', isItVisible);
 	//console.log("solts_arr", solts_arr);
 
-	//if (!rendered) {
-	var gtagFindElements = domQuery.find('div[id*="google_ad"]');
-	console.log(gtagFindElements);
-	store(_store, "gtag_impr", gtagFindElements.length);
-	if (callback) {
-		callback(gtagFindElements);
+	if (!rendered) {
+		var gtagFindElements = domQuery.find('div[id*="google_ad"]');
+		store(_store, "gtag_impr", gtagFindElements.length);
+		customEvent("impr", gtagFindElements.length);
+		if (callback) {
+			callback(gtagFindElements);
+		}
 	}
-	//}
-	//rendered = true;
+	rendered = true;
 };
 
 var listenForSlotsCallback = function (arr) {
-	var slotElementId = {};
-	arr.forEach(function (val) {
-		if (val.m) {
-			var id = val.m.getSlotElementId();
-			if (!slotElementId.hasOwnProperty(id)) {
-				slotElementId[id] = id;
+	if (isArray(arr)) {
+		var slotElementId = {};
+		arr.forEach(function (val) {
+			var type = val.firstElementChild.nodeName ? val.firstElementChild.nodeName : false;
+			if (type) {
+				slotElementId[val.parentElement.id] = { type: val.firstElementChild.nodeName, id: val.firstElementChild.id };
 			}
-		}
-	});
-	console.debug(slotElementId);
-	var gtagFindElements = domQuery.find('iframe[id*="google_ad"]');
-	console.log(gtagFindElements);
-	//console.log(getStore());
+		});
+		slotElementId.slots = Object.keys(slotElementId).length;
+		console.table(slotElementId);
+	}
 };
